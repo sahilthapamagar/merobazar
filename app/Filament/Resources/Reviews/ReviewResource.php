4bc299b0e61b7314bbb77d53ecb56e06c5c98fb1
magarsahil <sahilmagar275@gmail.com>
2026-08-08 16:42:44 +0545

    Add admin review management resource

---
+++ app/Filament/Resources/Reviews/ReviewResource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources\Reviews;
+
+use App\Filament\Resources\Reviews\Pages\EditReview;
+use App\Filament\Resources\Reviews\Pages\ListReviews;
+use App\Filament\Resources\Reviews\Schemas\ReviewForm;
+use App\Filament\Resources\Reviews\Tables\ReviewsTable;
+use App\Models\Review;
+use BackedEnum;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class ReviewResource extends Resource
+{
+    protected static ?string $model = Review::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ChatBubbleLeftRight;
+
+    public static function getRecordTitle(?Model $record): ?string
+    {
+        return $record ? 'Review #' . $record->id : null;
+    }
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['user.name', 'product.name', 'comment'];
+    }
+
+    public static function form(Schema $schema): Schema
+    {
+        return ReviewForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return ReviewsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListReviews::route('/'),
+            'edit' => EditReview::route('/{record}/edit'),
+        ];
+    }
+}
